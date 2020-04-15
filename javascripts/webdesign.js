86e300476f0ca246c085fcc84aa522f062035f32
ramon96 <rfm1@live.nl>
2020-04-15 12:39:20 +0200

    gyroscope test

--- javascripts/webdesign.js
+++ javascripts/webdesign.js
@@ -1,9 +1,52 @@
-DeviceOrientationEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
-    window.addEventListener('deviceorientation', (e) => {
-      // do something with e
-    })
-  }
-})
-.catch(console.error)
+let permissionGranted = false;
+let feedback = document.querySelector("#feedback");
+let button = document.querySelector("#permissionButton");
+
+if(typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function'){
+    //ios 13 device
+      
+      DeviceOrientationEvent.requestPermission()
+      .catch(() =>{
+        //button die om permission vraagt
+        button.addEventListener('click', function(){
+            requestAccess();
+        })
+
+      throw error;
+      })
+      .then(()=>{
+        permissionGranted = true;
+
+        window.requestAnimationFrame(gyroscope);
+      })
+      
+    }
+    else{
+     //non ios 13 device
+     feedback.innerHTML = "This is not an ios13 device :(";
+      
+    }  
+
+
+function requestAccess()
+{
+  DeviceOrientationEvent.requestPermission()
+    .then(res => {
+      if(response == 'granted'){
+        permissionGranted = true;
+      }else{
+        permissionGranted = false;
+      }
+  })
+  .catch(console.error);
+
+}
+
+function gyroscope(timestamp){
+
+
+
+    feedback.innerHTML = "gaat lekker "  +  rotationY + rotationX;
+
+    window.requestAnimationFrame(step);
+}
