fba0ed944fdfed8485f042186aecf2a134496eff
ramon96 <rfm1@live.nl>
2020-04-15 11:21:52 +0200

    we have some ios 13 issues

--- javascripts/webdesign.js
+++ javascripts/webdesign.js
@@ -1,8 +1,9 @@
-let gyroscope = new Gyroscope({frequency: 60});
-
-gyroscope.addEventListener('reading', e => {
-  console.log("Angular velocity along the X-axis " + gyroscope.x);
-  console.log("Angular velocity along the Y-axis " + gyroscope.y);
-  console.log("Angular velocity along the Z-axis " + gyroscope.z);
-});
-gyroscope.start();
+DeviceMotionEvent.requestPermission()
+.then(response => {
+  if (response == 'granted') {
+    window.addEventListener('devicemotion', (e) => {
+      // do something with e
+    })
+  }
+})
+.catch(console.error)
